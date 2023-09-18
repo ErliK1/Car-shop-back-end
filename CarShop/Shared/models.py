fc8009ff43bbd9545e01346d823229f72f0c299d
ErliK1 <ekuka21@epoka.edu.al>
2023-09-18 12:37:16 +0200

    First Commit

---
+++ CarShop/Shared/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+GENDER_CHOICES = [
+    ('M', 'MALE'),
+    ('F', 'FEMALE'),
+    ('OTHER', 'OTHER'),
+]
+
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=20, blank=True, verbose_name='Numri I Telefonit')
+    location_address = models.CharField(max_length=100, blank=True, verbose_name='Addresa(Lokacioni)')
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True, null=True, verbose_name='Gjinia')
+
+    class Meta(AbstractUser.Meta):
+        db_table = 'shared_user'
+
+
+class Mechanic(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    icon = models.ImageField()
+
+
+class BigUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
